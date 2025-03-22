655304a2fe1641eefb6690f1a9729c6098f046be
rajatsaha18 <editorrajatdiu@gmail.com>
2025-03-22 16:23:02 +0600

    First commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController;
+
+Route::get("/", [HomeController::class,"homePage"])->name("home");
+Route::get("/dashboard", [DashboardController::class,"dashboardPage"])->name('dashboardPage');
+Route::get("/categoryPage", [CategoryController::class,"categoryPage"])->name('categoryPage');
+Route::get("/userRegistration", [UserController::class,"userRegistrationPage"]);
+Route::get("/userLogin", [UserController::class,"userLoginPage"]);
+Route::get("/resetPassword", [UserController::class,"resetPasswordPage"]);
+Route::get("/sendOtp", [UserController::class,"sendOtpPage"]);
+Route::get("/verifyOtp", [UserController::class,"verifyOtpPage"]);
+Route::get('/userProfile', [UserController::class, 'profilePage']);
+
+
+
+
+
+
